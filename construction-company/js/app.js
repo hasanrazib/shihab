a9d025de49b1630f46b2bf3c17e7287dd6975dca
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-03-14 14:36:35 +0600

    Added Construction project, our skills, our mission, our team

--- construction-company/js/app.js
+++ construction-company/js/app.js
@@ -6,3 +6,21 @@ $(document).ready(function() {
   
       });
 });
+
+$('#team').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
