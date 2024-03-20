281d7a0a5f4a469c1aa39b1af2462d082c395feb
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-03-20 15:34:53 +0600

    Footer, Map, Contact, Nav, Partner
    Fixed Footer Section and added footer social
    added map, partner and contact section
    Fixed navbar

--- construction-company/js/app.js
+++ construction-company/js/app.js
@@ -24,3 +24,24 @@ $('#team').owlCarousel({
     }
 })
 
+$('#partners').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
