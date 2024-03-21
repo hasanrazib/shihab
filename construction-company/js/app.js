c74655e1a84cfb6711d6ba66a7b39d042a8fff35
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-03-21 15:25:23 +0600

    Added some sections and fixed some bugs

--- construction-company/js/app.js
+++ construction-company/js/app.js
@@ -30,17 +30,20 @@ $('#partners').owlCarousel({
     nav:false,
     dots: false,
     autoplay:true,
-    autoplayTimeout:5000,
+    autoplayTimeout:1000,
     autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
+        },
+        800:{
+            items:4
         },
         1000:{
-            items:3
+            items:5
         }
     }
 })
