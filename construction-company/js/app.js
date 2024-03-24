3fe9be5a0391363cdd60172a14aa5ebf11c4aa57
Saifur Rahman Shihab <thecodemaster26@gmail.com>
2024-03-24 15:40:20 +0600

    Added Recent News Section

--- construction-company/js/app.js
+++ construction-company/js/app.js
@@ -16,10 +16,27 @@ $('#team').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:3
+        }
+    }
+})
+
+$('#news').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
         }
     }
 })
